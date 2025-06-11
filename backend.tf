b3a08026297ad9ce745a1a0f11e4094e27336884
Oleh Kuchma <oleg.kuchma11@gmail.com>
2025-06-11 16:56:54 +0300

    backend change

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg-terraform-state"
     storage_account_name = "tfstatestoragebestrong"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "terraform.tfstate.${var.environment}"
   }
 }
--- envfetch.ps1
+++ envfetch.ps1
@@ -1,4 +1,4 @@
-$env:ARM_CLIENT_ID = (az keyvault secret show --name "TerraformClientId" --vault-name "bestrongkeyvault" --query "value" -o tsv)
-$env:ARM_CLIENT_SECRET = (az keyvault secret show --name "TerraformClientSecret" --vault-name "bestrongkeyvault" --query "value" -o tsv)
-$env:ARM_TENANT_ID = (az keyvault secret show --name "TerraformTenantId" --vault-name "bestrongkeyvault" --query "value" -o tsv)
-$env:ARM_SUBSCRIPTION_ID = (az keyvault secret show --name "TerraformSubscriptionId" --vault-name "bestrongkeyvault" --query "value" -o tsv)
+$env:ARM_CLIENT_ID = (az keyvault secret show --name "TerraformClientId" --vault-name "newbestrongkeyvault" --query "value" -o tsv)
+$env:ARM_CLIENT_SECRET = (az keyvault secret show --name "TerraformClientSecret" --vault-name "newbestrongkeyvault" --query "value" -o tsv)
+$env:ARM_TENANT_ID = (az keyvault secret show --name "TerraformTenantId" --vault-name "newbestrongkeyvault" --query "value" -o tsv)
+$env:ARM_SUBSCRIPTION_ID = (az keyvault secret show --name "TerraformSubscriptionId" --vault-name "newbestrongkeyvault" --query "value" -o tsv)
